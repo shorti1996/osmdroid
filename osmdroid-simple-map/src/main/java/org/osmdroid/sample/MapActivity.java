c470f7521ce6f7dd5da18f1a34a800a28b01b760
shorti1996 <shorti1996@gmail.com>
2019-03-30 14:07:34 +0100

    add location follow to simple map

--- osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.java
+++ osmdroid-simple-map/src/main/java/org/osmdroid/sample/MapActivity.java
@@ -1,12 +1,27 @@
 package org.osmdroid.sample;
 
+import android.Manifest;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.simplemap.R;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.util.List;
 
 /**
  * Bare bones osmdroid example
@@ -18,6 +33,9 @@ import org.osmdroid.views.MapView;
 public class MapActivity extends AppCompatActivity {
     private MapView mapView = null;
 
+    private IMapController mapController;
+    private MyLocationNewOverlay mLocationOverlay;
+
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
@@ -26,6 +44,48 @@ public class MapActivity extends AppCompatActivity {
         //TODO check permissions
         setContentView(R.layout.activity_main);
         mapView = findViewById(R.id.mapView);
+
+        // permissions
+        Dexter.withActivity(this)
+                .withPermissions(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.INTERNET,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_NETWORK_STATE
+                )
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        if (report.areAllPermissionsGranted()) {
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied()) {
+                        }
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+                        token.continuePermissionRequest();
+                    }
+                })
+                .onSameThread()
+                .check();
+
+        mapController = mapView.getController();
+        mapController.setZoom(5.0);
+
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
+
+        GpsMyLocationProvider provider = new GpsMyLocationProvider(getApplicationContext());
+        provider.addLocationSource(LocationManager.GPS_PROVIDER);
+        mLocationOverlay = new MyLocationNewOverlay(provider, mapView);
+        mapView.getOverlays().add(mLocationOverlay);
+        mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.ALWAYS);
+
+        mLocationOverlay.enableMyLocation();
+        mLocationOverlay.enableFollowLocation();
     }
 
     @Override
@@ -35,6 +95,9 @@ public class MapActivity extends AppCompatActivity {
                 PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
         if (mapView!=null) {
             mapView.onResume();
+        }
+        if (mLocationOverlay != null) {
+            mLocationOverlay.enableFollowLocation();
         }
     }
 
